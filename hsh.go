c5e677a1de550ef0daeceaae28f5cd981b784b9b
Tim Raymond <xtjraymondx@gmail.com>
2014-07-24 07:23:43 -0400

    Add readline
    Trying to rebuild readline is dumb, this instead uses the wrapper from
    shavac to get all of our history needs fulfilled

--- hsh.go
+++ hsh.go
@@ -2,8 +2,9 @@ package main
 
 import (
   "github.com/timraymond/hsh/hcommand"
+  "github.com/shavac/readline"
 
-	"bufio"
+	//"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -17,48 +18,51 @@ func main() {
 	}
 	app_name := os.Args[1]
 	ps := app_name + " > "
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print(ps)
+	//scanner := bufio.NewScanner(os.Stdin)
 
   // REPL
-	for scanner.Scan() {
-    input := scanner.Text()
-
-    // Parse out special commands
-		switch input {
-		case ":exit":
-			os.Exit(0)
-		case "":
-      fmt.Print(ps)
-			continue
-		case "console":
-			input = "run console"
-		}
-    cmd := hcommand.HerokuCmd(input, app_name)
-
-    //Channel Setup for process execution
-		done := make(chan error)
-		sig := make(chan os.Signal, 1)
-    // Setup kill notification
-    signal.Notify(sig, os.Interrupt)
-
-		go func() {
-			done <- cmd.Run()
-		}()
-		select {
-		case err := <-done:
-			if err != nil {
-				log.Print(err)
-			}
-			break
-		case s := <-sig:
-			err := cmd.Process.Signal(s)
-      <-done // wait for the subprocess to finish
-			if err != nil {
-				log.Fatal(err)
-			}
+	for {
+    switch input := readline.ReadLine(&ps); true {
+    case input == nil:
+      println()
       break
-		}
-    fmt.Print(ps)
+    case *input != "":
+      // Parse out special commands
+      switch *input {
+      case ":exit":
+        os.Exit(0)
+      case "":
+        fmt.Print(ps)
+        continue
+      case "console":
+        *input = "run console"
+      }
+      readline.AddHistory(*input)
+      cmd := hcommand.HerokuCmd(*input, app_name)
+
+      //Channel Setup for process execution
+      done := make(chan error)
+      sig := make(chan os.Signal, 1)
+      // Setup kill notification
+      signal.Notify(sig, os.Interrupt)
+
+      go func() {
+        done <- cmd.Run()
+      }()
+      select {
+      case err := <-done:
+        if err != nil {
+          log.Print(err)
+        }
+        break
+      case s := <-sig:
+        err := cmd.Process.Signal(s)
+        <-done // wait for the subprocess to finish
+        if err != nil {
+          log.Fatal(err)
+        }
+        break
+      }
+    }
 	}
 }
